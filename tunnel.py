df03958ad50dfebc29c05a6c28343b633273dc76
Aidan Walsh <abwalsh@princeton.edu>
2025-07-04 18:04:44 -0500

    more

--- tunnel.py
+++ tunnel.py
@@ -50,6 +50,7 @@ while True:
       if not(previous_move == "U" and item == "D") or (previous_move == "D" and item == "U") or (previous_move == "L" and item == "R") or (previous_move == "R" and item == "L") or (previous_move == "F" and item == "B") or (previous_move == "B" and item == "F"):
         executable.sendline(item)
         output = executable.readline()
+        output = str(output)
         if not("Cannot move that way" in output):
           previous_move = item 
           potential.append(item)
