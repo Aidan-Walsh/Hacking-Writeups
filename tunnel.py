7f8ffeaae6c48eb48a8f405265f2f3fe0dcff714
Aidan Walsh <abwalsh@princeton.edu>
2025-07-05 10:02:54 -0500

    more

--- tunnel.py
+++ tunnel.py
@@ -44,6 +44,10 @@ def update_position(char):
   elif char == "B":
     start[1] -= 1
     
+  print("statistics: ")
+  print(start)
+  print(location[start[0]][start[1]][start[2]])
+    
 def update_location():
   global location
   global start
