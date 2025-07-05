078a517a787ee8cd9eabb182c898c2adbb31c476
Aidan Walsh <abwalsh@princeton.edu>
2025-07-04 19:00:06 -0500

    more

--- tunnel.py
+++ tunnel.py
@@ -59,7 +59,7 @@ while True:
 
     if index == -1: 
 
-      if not(previous_move == "U" and item == "D") or (previous_move == "D" and item == "U") or (previous_move == "L" and item == "R") or (previous_move == "R" and item == "L") or (previous_move == "F" and item == "B") or (previous_move == "B" and item == "F"):
+      if not((previous_move == "U" and item == "D") or (previous_move == "D" and item == "U") or (previous_move == "L" and item == "R") or (previous_move == "R" and item == "L") or (previous_move == "F" and item == "B") or (previous_move == "B" and item == "F")):
 
         executable.sendline(item) 
         
