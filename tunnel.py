990d0a44197f41665bde47c3ae44f162dd6afeaf
Aidan Walsh <abwalsh@princeton.edu>
2025-07-05 09:58:07 -0500

    more

--- tunnel.py
+++ tunnel.py
@@ -4,10 +4,57 @@ import sys
 import time
 import numpy as np
 import codecs
+import copy
 
 
+def found_cycle(char):
+  global start
+  global location
+  copy_start = copy.deepcopy(start)
+  if char == "U":
+    copy_start[2] += 1
+  elif char == "D":
+    copy_start[2] -=1 
+  elif char == "L":
+    copy_start[0] -= 1
+  elif char == "R":
+    copy_start[0] += 1
+  elif char == "F":
+    copy_start[1] += 1
+  elif char == "B":
+    copy_start[1] -= 1
+    
+  if location[copy_start[0]][copy_start[1]][copy_start[2]]:
+    print("found cycle")
+  
+  return location[copy_start[0]][copy_start[1]][copy_start[2]]
+  
+def update_position(char):
+  global start
+  if char == "U":
+    start[2] += 1
+  elif char == "D":
+    start[2] -=1 
+  elif char == "L":
+    start[0] -= 1
+  elif char == "R":
+    start[0] += 1
+  elif char == "F":
+    start[1] += 1
+  elif char == "B":
+    start[1] -= 1
+    
+def update_location():
+  global location
+  global start
+  location[start[0]][start[1]][start[2]] = True
 
 # repeatedly send lines
+
+# save where we go 
+size = 100
+location = [[[False] * size] * size] * size
+start = [size/2,size/2,size/2]
 
 payload = ["U","D","L","R","F","B"]
 quit = "Q"
@@ -22,6 +69,7 @@ executable = process('./tunnel')
 while True:
   if not(found_route):
     executable = process('./tunnel')
+    location = [[[False] * size] * size] * size
     first = executable.readline()
     second = executable.readline
     print(str(first))
@@ -53,6 +101,8 @@ while True:
       
     for character in potential:
       executable.sendline(character)
+      update_position(character)
+      update_location()
 
   for ind in range(len(payload)):
     item = payload[ind]
@@ -71,10 +121,13 @@ while True:
      
         
         print("output: " + output + " with input: " + item)
-        if not("Cannot move that way" in output):
+        if not("Cannot move that way" in output) and not(found_cycle(item)):
           found_route = True
           previous_move = item 
           potential += item
+          
+          
+          
           
           print("working with path: " + potential)
           if "break into the vault" in output:
